1ea69f1600822b5b9572f5ee0a6ce06571351e9c
Paul Kravchenko <pk@sitecore.net>
2018-10-01 17:02:47 +0300

    Project commited

---
+++ src/Sitecore.Support.256440/ItemSearchSettingsPlugin.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.DataExchange;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.DataExchange.Providers.Sc.Plugins
+{
+  public class ItemSearchSettingsPlugin: IPlugin
+  {
+    public int PageSize { get; set; }
+  }
+}
